5397deba2c9fe2b301509a6885b382fc5abb369a
LiranBratt2121 <brattlirannin@gmail.com>
2023-01-03 23:55:18 +0200

    change const values

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    // motor controller 
+    public static final int k_back_left_motor = 1;
+    public static final int k_back_right_motor = 2;
+    public static final int k_front_left_motor = 3;
+    public static final int k_front_right_motor = 4;
+
+    // joystick
+    public static final int k_joystick_port = 0;
+
+    // pid
+    public static final float k_p = 0.2f; 
+    public static final float k_i = 0.0f; 
+    public static final float k_d = 0.5f; 
+
+
+    // service
+    public static int count = 0; 
+
+
+}
+
+    
